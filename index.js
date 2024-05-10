0c3a3b4674662cd5d602ffb811c4beb750b38dbb
Tobias Eriksson <toberi@outlook.com>
2024-05-10 02:32:42 +0200

    added support for enter click addNote, style updates, sort by completed, removed addNote button.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", getNotes);
 
-const addNoteBtn = document.getElementById("add-note");
 const notesList = document.getElementById("notes-list");
 const noteInput = document.getElementById("note-input");
 
-addNoteBtn.onclick = function () {
+noteInput.addEventListener("keydown", function (e) {
   const inputValue = noteInput.value.trim();
-  if (inputValue !== "") {
-    addNote(inputValue);
-    saveNotes();
+  if (e.key === "Enter") {
+    if (inputValue !== "") {
+      addNote(inputValue);
+      saveNotes();
+    }
   }
-};
+});
 
 function addNote(text, isCompleted = false) {
   const li = document.createElement("li");
@@ -31,14 +32,12 @@ function addNote(text, isCompleted = false) {
   li.appendChild(deleteNoteBtn);
 }
 
-function completeNote(e) {
-  if (e.target.tagName !== "BUTTON" && e.target.tagName !== "SPAN") {
-    const li = this.closest("li");
-    const textSpan = this.querySelector("p");
-    li.classList.toggle("li-completed");
-    textSpan.classList.toggle("p-completed");
-    saveNotes();
-  }
+function completeNote() {
+  const li = this.closest("li");
+  const textSpan = this.querySelector("p");
+  li.classList.toggle("li-completed");
+  textSpan.classList.toggle("p-completed");
+  saveNotes();
 }
 
 function deleteNote(e) {
@@ -57,7 +56,11 @@ function saveNotes() {
       note.closest("li").classList.contains("li-completed");
     notes.push({ text, isCompleted });
   });
+  notes.sort((a, b) => {
+    return a.isCompleted === b.isCompleted ? 0 : a.isCompleted ? 1 : -1;
+  });
   localStorage.setItem("notes", JSON.stringify(notes));
+  refreshNotes();
 }
 
 function getNotes() {
@@ -67,4 +70,15 @@ function getNotes() {
       addNote(note.text, note.isCompleted);
     });
   }
+}
+
+function clearNotes() {
+  while (notesList.firstChild) {
+    notesList.removeChild(notesList.firstChild);
+  }
+}
+
+function refreshNotes() {
+  clearNotes();
+  getNotes();
 }
