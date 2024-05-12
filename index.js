ae5e6950fc1a45d31d714eace9d5e51bf94b39ad
Tobias Eriksson <toberi@outlook.com>
2024-05-12 02:19:07 +0200

    refactor js

--- index.js
+++ index.js
@@ -3,80 +3,75 @@ document.addEventListener("DOMContentLoaded", getNotes);
 const notesList = document.getElementById("notes-list");
 const noteInput = document.getElementById("note-input");
 
-noteInput.addEventListener("keydown", function (e) {
-  const inputValue = noteInput.value.trim();
-  if (e.key === "Enter" && inputValue !== "") {
-    addNote(inputValue);
-    saveNotes();
+noteInput.addEventListener("keydown", handleNoteInput);
+
+function handleNoteInput(e) {
+  if (e.key === "Enter") {
+    const inputValue = noteInput.value.trim();
+    if (inputValue) {
+      addNote(inputValue);
+      saveNotes();
+      noteInput.value = "";
+    }
   }
-});
+}
 
 function addNote(text, isCompleted = false) {
   const li = document.createElement("li");
   const textSpan = document.createElement("p");
   textSpan.textContent = text;
-  if (isCompleted) {
-    li.classList.add("li-completed");
-    textSpan.classList.add("p-completed");
-  }
   li.appendChild(textSpan);
   notesList.appendChild(li);
-  noteInput.value = "";
-  li.addEventListener("click", completeNote);
 
+  if (isCompleted) {
+    li.classList.toggle("li-completed");
+    textSpan.classList.toggle("p-completed");
+  }
+
+  li.addEventListener("click", () => markAsCompleted(li, textSpan));
+  appendDeleteButton(li);
+}
+
+function appendDeleteButton(li) {
   const deleteNoteBtn = document.createElement("button");
-  deleteNoteBtn.innerHTML = "<span>&times;</span>";
-  deleteNoteBtn.addEventListener("click", deleteNote);
+  deleteNoteBtn.innerHTML = `<span>&times;</span>`;
+  deleteNoteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    notesList.removeChild(li);
+    saveNotes();
+  });
   li.appendChild(deleteNoteBtn);
 }
 
-function completeNote() {
-  const li = this.closest("li");
-  const textSpan = this.querySelector("p");
+function markAsCompleted(li, textSpan) {
   li.classList.toggle("li-completed");
   textSpan.classList.toggle("p-completed");
   saveNotes();
-}
-
-function deleteNote(e) {
-  e.stopPropagation();
-  const note = e.target.closest("li");
-  notesList.removeChild(note);
-  saveNotes();
+  refreshNotes();
 }
 
 function saveNotes() {
-  const notes = [];
-  document.querySelectorAll("#notes-list li").forEach((note) => {
+  const notes = Array.from(notesList.querySelectorAll("li")).map((note) => {
     const text = note.querySelector("p").textContent;
-    const isCompleted =
-      note.querySelector("p").classList.contains("p-completed") &&
-      note.closest("li").classList.contains("li-completed");
-    notes.push({ text, isCompleted });
+    const isCompleted = note.classList.contains("li-completed");
+    return { text, isCompleted };
   });
   notes.sort((a, b) => {
     return a.isCompleted === b.isCompleted ? 0 : a.isCompleted ? 1 : -1;
   });
   localStorage.setItem("notes", JSON.stringify(notes));
-  refreshNotes();
 }
 
 function getNotes() {
-  const notes = JSON.parse(localStorage.getItem("notes"));
-  if (notes) {
-    notes.forEach((note) => {
-      addNote(note.text, note.isCompleted);
-    });
-  }
+  const notes = JSON.parse(localStorage.getItem("notes")) ?? [];
+  notes.forEach((note) => addNote(note.text, note.isCompleted));
 }
 
-function clearNotes() {
-  while (notesList.firstChild) {
-    notesList.removeChild(notesList.firstChild);
-  }
+function clearNotesList() {
+  notesList.innerHTML = "";
 }
 
 function refreshNotes() {
-  clearNotes();
+  clearNotesList();
   getNotes();
 }
